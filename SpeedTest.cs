fbf60d23070637b4fcda3e0f3c72c2480492ac94
Mark Aldrich <44414229+MarkEAldrichJr@users.noreply.github.com>
2024-09-07 16:34:51 -0400

    Create SpeedTest.cs
    Moved script from file to repository.
    Added some comments to each structural change type

---
+++ SpeedTest.cs
@@ -0,0 +1,151 @@
+//quick test of structural change speeds with ECB, Entity Manager, and batched Entity Manager
+//Mark Aldrich
+
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+
+namespace SpeedTest
+{    
+    public struct SpeedTestEntityTag : IComponentData {}
+    public struct SpeedTestRemovableEntityTag : IComponentData {}
+
+    [UpdateBefore(typeof(SpeedTestAddSystem))]
+    public partial struct SpeedTestSpawnSystem : ISystem
+    {
+        private EntityArchetype _speedTestUnitArchetype;
+        
+        public void OnCreate(ref SystemState state)
+        {
+            _speedTestUnitArchetype = state.EntityManager
+                .CreateArchetype(typeof(SpeedTestEntityTag));
+        }
+        
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            //only run if framerate is greater than FPS value
+            const float fps = 120f;
+            var deltaTime = SystemAPI.Time.DeltaTime;
+            if (deltaTime > 1f / fps) return;
+
+            //comment out entity manager and uncomment ecb lines to compare
+            //var ecb = new EntityCommandBuffer(Allocator.Temp);
+            for (var i = 0; i < 1000; i++)
+            {
+                //ecb.CreateEntity(_speedTestUnitArchetype);
+                state.EntityManager.CreateEntity(_speedTestUnitArchetype);
+            }
+            //ecb.Playback(state.EntityManager);
+        }
+    }
+    
+    [UpdateBefore(typeof(SpeedTestRemoveSystem))]
+    public partial struct SpeedTestAddSystem : ISystem
+    {
+        private EntityQuery _query;
+        private ComponentType _componentType;
+        
+        public void OnCreate(ref SystemState state)
+        {
+            var builder = new EntityQueryBuilder(Allocator.Temp)
+                .WithAll<SpeedTestEntityTag>()
+                .WithNone<SpeedTestRemovableEntityTag>();
+            _query = state.GetEntityQuery(builder);
+
+            _componentType = typeof(SpeedTestRemovableEntityTag);
+        }
+
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            /*
+            //add components with ECB
+            var ecb = new EntityCommandBuffer(Allocator.Temp);
+            foreach (var (_, e) in SystemAPI
+                         .Query<RefRO<SpeedTestEntityTag>>()
+                         .WithNone<SpeedTestRemovableEntityTag>()
+                         .WithEntityAccess())
+            {
+                ecb.AddComponent<SpeedTestRemovableEntityTag>(e);
+            }
+            ecb.Playback(state.EntityManager);
+            */
+            
+            /*
+            //add components with EntityManager
+             var withOutSpeedTag = new NativeList<Entity>(Allocator.Temp);
+            foreach (var (_, e) in SystemAPI
+                         .Query<RefRO<SpeedTestEntityTag>>()
+                         .WithNone<SpeedTestRemovableEntityTag>()
+                         .WithEntityAccess())
+            {
+                withOutSpeedTag.Add(e);
+            }
+            foreach (var e in withOutSpeedTag)
+            {
+                state.EntityManager.AddComponent<SpeedTestRemovableEntityTag>(e);
+            }
+            */
+
+            /*
+            //Batched EntityManager Adder
+            state.EntityManager.AddComponent(_query, _componentType);
+            */
+        }
+    }
+    
+    public partial struct SpeedTestRemoveSystem : ISystem
+    {
+        private EntityQuery _query;
+        private ComponentType _componentType;
+        
+        public void OnCreate(ref SystemState state)
+        {
+            var builder = new EntityQueryBuilder(Allocator.Temp)
+                .WithAll<SpeedTestEntityTag, SpeedTestRemovableEntityTag>();
+            _query = state.GetEntityQuery(builder);
+            
+            _componentType = typeof(SpeedTestRemovableEntityTag);
+        }
+        
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            /*
+            //remove components with ECB
+            var ecb = new EntityCommandBuffer(Allocator.Temp);
+            foreach (var (_, e) in SystemAPI
+                         .Query<RefRO<SpeedTestEntityTag>>()
+                         .WithAll<SpeedTestRemovableEntityTag>()
+                         .WithEntityAccess())
+            {
+                ecb.RemoveComponent<SpeedTestRemovableEntityTag>(e);
+            }
+            ecb.Playback(state.EntityManager);
+            */
+            
+            /*
+            //remove components with EntityManager
+            var withSpeedTag = new NativeList<Entity>(Allocator.Temp);
+            
+            foreach (var (_, e) in SystemAPI
+                         .Query<RefRO<SpeedTestEntityTag>>()
+                         .WithAll<SpeedTestRemovableEntityTag>()
+                         .WithEntityAccess())
+            {
+                withSpeedTag.Add(e);
+            }
+            foreach (var e in withSpeedTag)
+            {
+                state.EntityManager.AddComponent<SpeedTestRemovableEntityTag>(e);
+            }
+            */
+
+            /*
+            //batched component removal
+            state.EntityManager.RemoveComponent(_query, _componentType);
+            */
+        }
+    }
+}
